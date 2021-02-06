8837cacddf0a6fe7dc09d4314886947e75990e10
cezarcalixto <cezarcalixto222@gmail.com>
2021-02-05 21:08:17 -0300

    adicionando lista automaticamente e button

--- script.js
+++ script.js
@@ -21,4 +21,44 @@ function initTabNav(){
   }
   initTabNav();
   
+var ul = document.getElementById('myList')
+console.log(ul)
+var li;
+var itemId;
+var item;
+
+addTask = function(){
+  if (document.getElementById('task').value != ''){
+    item = document.getElementById('task');
+    itemId = ul.childElementCount;
+    li = createItemElement(item.value, itemId);
+    li.appendChild(createRemoveBtn(itemId));
+    ul.appendChild(li)
+    item.value='';
+  }
+}
+
+removeTask = function(itemId){
+  for (i = 0 ; i < ul.children.length ; i++){
+    if (ul.children[i].getAttribute("index") == itemId){
+      ul.children[i].remove();
+    }
+  }
+}
+
+createItemElement = function(itemValue, itemId){
+let li = document.createElement('li')
+li.setAttribute('index', itemId)
+li.appendChild(document.createTextNode(itemValue));
+return li;
+}
+
+createRemoveBtn = function(itemId){
+let btn = document.createElement('button')
+btn.setAttribute('onclick', "removeTask("+itemId+")");
+btn.innerHTML = 'X';
+btn.className += 'btn btn-light'
+btn.classList.add('col-md-12')
+return btn
+}
   
